a0894c80ce1a6c624f9115c420423e4d98d85ca7
Ihar Tsimafeyeu <ColdSun1395@gmail.com>
2022-09-08 22:47:46 +0300

    add pod 'Alamofire'

--- MagicTaskManager/Controller/Tabbar/MainTabBarController.swift
+++ MagicTaskManager/Controller/Tabbar/MainTabBarController.swift
@@ -2,8 +2,8 @@ import UIKit
 
 final class MainTabBarController: UITabBarController {
     
-    private let toDoVC = UINavigationController(rootViewController: TodoViewController)
-    private let notesTableViewVC = UINavigationController(rootViewController: NotesTableViewController)
+    private let toDoVC = UINavigationController(rootViewController: TodoViewController())
+    private let notesTableViewVC = UINavigationController(rootViewController: NotesTableViewController())
     
     override func viewDidLoad() {
         super.viewDidLoad()
