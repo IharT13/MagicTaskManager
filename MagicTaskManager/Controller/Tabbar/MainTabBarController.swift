fdde9cc4e6332f59b254da44fb47108f32ab4eca
Ihar Tsimafeyeu <ColdSun1395@gmail.com>
2022-09-17 15:45:33 +0300

    Add NetworkingManager with Alamofire

--- MagicTaskManager/Controller/Tabbar/MainTabBarController.swift
+++ MagicTaskManager/Controller/Tabbar/MainTabBarController.swift
@@ -2,10 +2,16 @@ import UIKit
 
 final class MainTabBarController: UITabBarController {
     
+    private var todoS = [ToDo]()
+    
     private let toDoVC = UINavigationController(rootViewController: TodoViewController())
     private let notesTableViewVC = UINavigationController(rootViewController: NotesTableViewController())
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        NetworkingManager.shared.getData { todo in
+            self.todoS = todo
+            print(self.todoS)
+        }
     }
 }
---
+++ MagicTaskManager/Manager/NetworkingManager.swift
@@ -0,0 +1,28 @@
+import Foundation
+import Alamofire
+
+final class NetworkingManager {
+    
+    static let shared = NetworkingManager()
+    
+    private enum Constants {
+        static let baseURL = "https://jsonplaceholder.typicode.com"
+    }
+    
+    private enum EndPoints {
+        static let todos = "/todos"
+    }
+    
+    func getData (completion: @escaping(([ToDo]) -> Void)) {
+        AF.request(Constants.baseURL + EndPoints.todos).responseDecodable(of: [ToDo].self) { response in
+            switch response.result {
+            case .success(let model):
+                completion(model)
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+    
+    private init() {}
+}
